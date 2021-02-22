5311308b76ab311ac7c057490a02b3c69dc74c18
leem3209@gmail.com <leem3209@gmail.com>
2021-02-23 00:24:30 +0800

    flowerpedia and some css

--- routes/flowerpedia.js
+++ routes/flowerpedia.js
@@ -1,3 +1,57 @@
+
+
+
 exports.view = function(req, res){
-    res.render('flowerpedia');
+    res.render('flowerpedia', {
+    	'flower':[
+	    	{	'name': 'Sweet Flower',
+	    		'image': 'sweetFlower.png',
+	    		'id': 'sweetFlower'
+	    	},
+	    	{	'name': 'Silk Flower',
+	    		'image': 'silkFlower.png',
+	    		'id': 'silkFlower'
+	    	},
+	    	{	'name': 'Qingxin',
+	    		'image': 'qingxin.png',
+	    		'id': 'qingxin'
+	    	},
+	    	{	'name': 'Gaze Lily',
+	    		'image': 'glazeLily.png',
+	    		'id': 'glazeLily'
+	    	},
+	    	{	'name': 'Cecilia',
+	    		'image': 'cecilia.png',
+	    		'id': 'cecilia'
+	    	},
+	    	{	'name': 'Exile',
+	    		'image': 'exile.png',
+	    		'id': 'exile'
+	    	},
+	    	{	'name': 'Sweet Flower',
+	    		'image': 'sweetFlower.png',
+	    		'id': 'sweetFlower'
+	    	},
+	    	{	'name': 'Silk Flower',
+	    		'image': 'silkFlower.png',
+	    		'id': 'silkFlower'
+	    	},
+	    	{	'name': 'Qingxin',
+	    		'image': 'qingxin.png',
+	    		'id': 'qingxin'
+	    	},
+	    	{	'name': 'Gaze Lily',
+	    		'image': 'glazeLily.png',
+	    		'id': 'glazeLily'
+	    	},
+	    	{	'name': 'Cecilia',
+	    		'image': 'cecilia.png',
+	    		'id': 'cecilia'
+	    	},
+	    	{	'name': 'Exile',
+	    		'image': 'exile.png',
+	    		'id': 'exile'
+	    	},
+    	]
+    })
   };
