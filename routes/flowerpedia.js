c0c2080b69cb9fa6098e4ca4dc1049b6ad9cfb76
leem3209@gmail.com <leem3209@gmail.com>
2021-03-02 18:15:49 +0800

    change json

--- routes/flowerpedia.js
+++ routes/flowerpedia.js
@@ -1,57 +1,8 @@
-
+var data = require("../flowerData.json");
 
 
 exports.view = function(req, res){
-    res.render('flowerpedia', {
-    	'flower':[
-	    	{	'name': 'Sweet Flower',
-	    		'image': 'sweetFlower.png',
-	    		'id': 'sweetFlower'
-	    	},
-	    	{	'name': 'Silk Flower',
-	    		'image': 'silkFlower.png',
-	    		'id': 'silkFlower'
-	    	},
-	    	{	'name': 'Qingxin',
-	    		'image': 'qingxin.png',
-	    		'id': 'qingxin'
-	    	},
-	    	{	'name': 'Glaze Lily',
-	    		'image': 'glazeLily.png',
-	    		'id': 'glazeLily'
-	    	},
-	    	{	'name': 'Cecilia',
-	    		'image': 'cecilia.png',
-	    		'id': 'cecilia'
-	    	},
-	    	{	'name': 'Exile',
-	    		'image': 'exile.png',
-	    		'id': 'exile'
-	    	},
-	    	{	'name': 'Sweet Flower',
-	    		'image': 'sweetFlower.png',
-	    		'id': 'sweetFlower'
-	    	},
-	    	{	'name': 'Silk Flower',
-	    		'image': 'silkFlower.png',
-	    		'id': 'silkFlower'
-	    	},
-	    	{	'name': 'Qingxin',
-	    		'image': 'qingxin.png',
-	    		'id': 'qingxin'
-	    	},
-	    	{	'name': 'Glaze Lily',
-	    		'image': 'glazeLily.png',
-	    		'id': 'glazeLily'
-	    	},
-	    	{	'name': 'Cecilia',
-	    		'image': 'cecilia.png',
-	    		'id': 'cecilia'
-	    	},
-	    	{	'name': 'Exile',
-	    		'image': 'exile.png',
-	    		'id': 'exile'
-	    	},
-    	]
-    })
+    res.render('flowerpedia', 
+    	data
+    )
   };
--- routes/friends.js
+++ routes/friends.js
@@ -1,23 +1,8 @@
-
+var data = require("../friendsData.json");
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
-    res.render('friends', {
-      'friends': [
-        { 'name': 'Audrey Fu',
-          'image': 'friendo1.png',
-          'id': 'Audrey'
-        },
-        { 'name': 'Mandy Lee',
-          'image': 'friendo2.png',
-          'id': 'Mandy'
-        },
-        { 'name': 'Avery Hom',
-        'image': 'friendo3.png',
-        'id': 'Avery'
-        }
-      ]  
-    });
+    res.render('friends', data);
   };
